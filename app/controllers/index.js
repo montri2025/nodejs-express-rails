2ef7c31dcc9c229071067e2367a30cbf74bab1ec
tong <montri2025@gmail.com>
2014-06-05 17:35:05 +0700

    Edit Time out

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'BRENM','fullBrand':'Bootsrap Rails Express NodeJS Mongodb' });
 });
 
 module.exports = router;
